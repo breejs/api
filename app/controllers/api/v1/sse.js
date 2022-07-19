27d81fab64990e3335eb79e1667fe756c0ce73d8
Taylor Schley <taylorschley@me.com>
2022-07-18 22:47:11 -0500

    feat: add status ping

--- app/controllers/api/v1/sse.js
+++ app/controllers/api/v1/sse.js
@@ -1,5 +1,26 @@
 async function connect(ctx) {
   if (ctx.sse) {
+    // likely not the best way to do this
+    // TODO: fork koa-sse and move this into the ping interval
+    // runs every 60s
+    const interval = setInterval(() => {
+      const connected = ctx.sse.send({
+        event: 'status',
+        data: isActive(ctx)
+      });
+
+      console.log('connected');
+
+      // clear the interval if the client is disconnected
+      if (!connected) {
+        clearInterval(interval);
+      }
+    }, 60_000);
+    ctx.sse.send({
+      event: 'status',
+      data: isActive(ctx)
+    });
+
     // send bree events over sse
     for (const event of ['worker created', 'worker deleted']) {
       ctx.bree.on(event, (name) => {
@@ -9,8 +30,18 @@ async function connect(ctx) {
 
     ctx.sse.on('close', () => {
       ctx.logger.error('SSE closed');
+
+      clearInterval(interval);
     });
   }
+}
+
+function isActive(ctx) {
+  return Boolean(
+    ctx.bree.workers.size > 0 ||
+      ctx.bree.timeouts.size > 0 ||
+      ctx.bree.intervals.size > 0
+  );
 }
 
 module.exports = { connect };
